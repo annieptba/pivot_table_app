a38dae7d1585bf0e12b00d302c95665bad1b7fe8
annieptba <61258785+annieptba@users.noreply.github.com>
2021-12-02 16:46:26 -0500

    asfqwr

--- app.py
+++ app.py
@@ -9,45 +9,47 @@ import pandas as pd
 
 df = pd.read_csv('https://bit.ly/elements-periodic-table')
 
+app = dash.Dash(__name__)
+
 # create columns that have unique values
-unique_col = ["AtomicNumber", "Element", "Symbol", "NumberOfProtons", "NumberOfElectrons"]
+unique_col_dict =  ["AtomicNumber", "Element", "Symbol", "NumberOfProtons", "NumberOfElectrons"]
+
 
 # create columns that don't have unique values
-not_unique_col = ['AtomicMass', 'NumberOfNeutrons', 'Period', 'Group', 'Phase', 'Radioactive', 'Natural', 'Metal', 'Nonmetal',
-                  'Metalloid', 'Type', 'AtomicRadius', 'Electronegativity', 'FirstIonization', 'Density', 'MeltingPoint',
-                  'BoilingPoint', 'NumberOfIsotopes', 'Discoverer', 'Year', 'SpecificHeat', 'NumberOfShells', 'NumberOfValence']
+not_unique_col_dict =  ['AtomicMass', 'NumberOfNeutrons', 'Period', 'Group', 'Phase', 'Radioactive',
+'Natural', 'Metal', 'Nonmetal', 'Metalloid', 'Type', 'AtomicRadius', 'Electronegativity',
+'FirstIonization', 'Density', 'MeltingPoint', 'BoilingPoint', 'NumberOfIsotopes',
+'Discoverer', 'Year', 'SpecificHeat', 'NumberOfShells', 'NumberOfValence']
 
-app = dash.Dash(__name__)
+# make an identity function
+def identity(x): return x
 
 # make app layout
 app.layout = html.Div([
    html.Div([
       dcc.Dropdown(
       id='index_selection',
-      options=[{'label': col, 'value': col} for col in unique_col],
-      value=None
+      options=[{'label': col, 'value': col} for col in unique_col_dict]
       )
    ]),
    html.Div(id='index-output'),
    html.Div([
       dcc.Dropdown(
       id='column_selection',
-      options=[{'label': col, 'value': col} for col in unique_col],
-      value=None
+      options=[{'label': col, 'value': col} for col in unique_col_dict]
       )
    ]),
    html.Div(id='column-output'),
    html.Div([
       dcc.Dropdown(
       id='value_selection',
-      options=[{'label': col, 'value': col} for col in not_unique_col],
-      value=None
+      options=[{'label': col, 'value': col} for col in not_unique_col_dict]
       )
    ]),
    html.Div(id='value-output'),
    html.Div([
       dash_table.DataTable(
-      id='pivot-table',
+      id='pivot_table',
       columns=[{"name": i, "id": i} for i in df.columns],
       data=df.to_dict('records')
       )
@@ -56,19 +58,19 @@ app.layout = html.Div([
 
 # app callback
 @app.callback(
-   Output(component_id='pivot_table', component_property='data'),
+   [
+      Output(component_id='pivot_table', component_property='columns'),
+      Output(component_id='pivot_table', component_property='data')],
    [
       Input(component_id='index_selection', component_property='value'),
       Input(component_id='column_selection', component_property='value'),
       Input(component_id='value_selection', component_property='value')
    ]
 )
-
-# make an identity function
-def identity(x): return x
-
-# make function to make pivot table
-def output_table(df, index_col, columns_col, values_col):
+# make a pivot table
+def make_pivot_table(index_col, columns_col, values_col):
+   if index_col is None or columns_col is None or values_col is None:
+      return ([{"name": i, "id": i} for i in df.columns], f.to_dict('records'))
    not_unique_cols = [ col for col in df.columns if df[col].is_unique == False]
    if index_col == columns_col or index_col == values_col or columns_col ==values_col:
       print("row and column index needs to be different")
@@ -77,10 +79,13 @@ def output_table(df, index_col, columns_col, values_col):
             index= index_col,
             columns= columns_col,
             values= values_col,
-            aggfunc=identity,
+            aggfunc=identity
       )
-      return df_piv
+   return ([{"name": i, "id": i} for i in df_piv.columns], df_piv.to_dict('records'))
+
+#def update_pivot_table():
+    #return make_pivot_table(df, 'Period', 'Group', 'Discoverer') # needs to be replaced
+
 
 # run app
 app.run_server(debug=True, host="0.0.0.0")
-
